2c83b4acf128922b4a551d25f1b8ac28edef345d
gerry <gerryyhonatan29@gmail.com>
2025-03-18 22:50:34 +0700

    feat: conditional rendering

--- src/Pages/login.jsx
+++ src/Pages/login.jsx
@@ -1,12 +1,10 @@
-import { Link } from "react-router-dom"
 import FormLogin from "../components/Fragments/FormLogin"
 import AuthLayouts from "../components/Layouts/AuthLayouts"
 
 const LoginPage = () => {
     return (
-        <AuthLayouts title="Login">
-            <FormLogin/>
-            <p className="text-sm mt-5 text-center">Don't have an account? <Link to="/register" className="font-bold text-blue-600">Register</Link></p>
+        <AuthLayouts title="Login" type="login">
+            <FormLogin/> 
         </AuthLayouts>
     )
 }
