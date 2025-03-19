fa08887a5de290f69cb64c6868c747b5e93d5fe1
gerry <gerryyhonatan29@gmail.com>
2025-03-19 21:40:29 +0700

    feat: rendering lists

--- src/Pages/products.jsx
+++ src/Pages/products.jsx
@@ -1,16 +1,37 @@
 import Button from "../components/Elements/Button"
 import CardProduct from "../components/Fragments/CardProduct"
 
+const products = [
+    {
+        id: 1,
+        name: "Sepatu Baru",
+        price: "Rp. 1.000.000",
+        image: "/public/images/Product1.jpg",
+        description: "Lorem ipsum dolor sit amet."
+    },
+    {
+        id: 2,
+        name: "Sepatu Lama",
+        price: "Rp. 1.200.000",
+        image: "/public/images/Product1.jpg",
+        description: "Lorem ipsum dolor sit amet. lorem lorem lorem lorem lorem lorem lorem lorem lorem"
+    }
+]
+
 const ProductsPage = () => {
     return (
         <div className="flex justify-center py-5">
-            <CardProduct>
-                <CardProduct.Header/>
-                <CardProduct.Body title="Sepatu Baru">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias fugiat in maiores earum voluptatum sapiente!
+            
+            {products.map((product) => (
+                <CardProduct>
+                <CardProduct.Header image={product.image}/>
+                <CardProduct.Body name={product.name}>
+                    {product.description}
                 </CardProduct.Body>
-                <CardProduct.Footer/>
-            </CardProduct>
+                <CardProduct.Footer price={product.price}/>
+                </CardProduct>
+            )
+            )}
         </div>
     )
 }
